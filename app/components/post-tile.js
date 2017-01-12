a3bca4f1af7b64d72a5390ab4c9fbd7ad7029c8e
Erik Killops <erik.killops@gmail.com>
2017-01-11 16:06:12 -0800

    make individual post route function and accessible

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    viewPost(postId) {
+      this.sendAction('viewPost', postId);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
+  },
+  actions: {
+    viewPost(postId) {
+      this.transitionTo('post', postId);
+    }
   }
 });
